64f8d794dd70a857fb05a10c44454c9157b85ed9
Karthik <chejerlakarthik@github.com>
2018-09-01 23:27:44 +1000

    Added command support using yargs

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
+const titleOptions = {
+    describe : 'Title for the note',
+    demand: true,
+    alias : 't'
+};
+
+const messageOptions = {
+    describe : 'Message for the note',
+    demand : true,
+    alias : 'm'
+};
+
 const notes = require('./notes');
-const args = yargs.argv;
+const args = yargs
+                .command('add', 'Add a new title', {
+                    title : titleOptions,
+                    message : messageOptions
+                })
+                .command('list', 'List all titles', {})
+                .command('remove', 'Remove note by title', {
+                    title : titleOptions
+                })
+                .command('findOne', 'Find note by title', {
+                    title : titleOptions
+                })
+                .help()
+                .argv;
 
 const command = args._[0];
 const title = args.title;
